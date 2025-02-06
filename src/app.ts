bc7ce156e94a53012841f658d057b34092c29460
Siffster85 <ylsaxon@googlemail.com>
2025-02-06 10:55:45 +0000

    Trying to fix cors 1

--- src/app.ts
+++ src/app.ts
@@ -16,13 +16,8 @@ const app = express();
 const allowedOrigins = process.env.REACT_APP_FRONTEND_URL;
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins?.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: allowedOrigins,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
 }));
 
 app.use(morgan("dev"));
