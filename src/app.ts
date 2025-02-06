520afd5ee666cc2c6cba8898e5233e41e1a3c9db
Siffster85 <ylsaxon@googlemail.com>
2025-02-06 17:12:46 +0000

    Reverting

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ app.use(
   cors({
     origin: "https://ylskanban.netlify.app/",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    //credentials: true,
+    credentials: true,
 }),
 );
 
