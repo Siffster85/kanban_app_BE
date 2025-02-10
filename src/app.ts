4729af5e5fc59ddf418973284723409a698fb810
Siffster85 <ylsaxon@googlemail.com>
2025-02-10 16:38:19 +0000

    tweaking origin for cors

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://ylskanban.netlify.app",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
 }),
